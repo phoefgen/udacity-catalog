613c231b9c56de5d9e2300aa1744e274c722f4d9
Paul Hoefgen <paul.hoefgen+GH@gmail.com>
2016-01-13 22:16:37 -0800

    fix bug in provisioner

--- pg_config.sh
+++ pg_config.sh
@@ -11,8 +11,8 @@ pip install flask-httpauth
 pip install oauth2client
 pip install Flask-WTF
 
-# initialise database:
-python /vagrant/drtysnow/db/dbsetup.py
+# This line deletes all the data, and sets up a new Db with test lorem ipsum.
+bash /vagrant/drtysnow/test_init.sh
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
