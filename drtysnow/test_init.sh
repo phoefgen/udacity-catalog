8bf2016c41e2f8f2224283a392380b083819e9fb
Paul Hoefgen <paul.hoefgen+GH@gmail.com>
2016-01-13 21:34:53 -0800

    clean up docs, ready for submission testing

---
+++ drtysnow/test_init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Generate fake data for testing the drtysnow app.
+
+echo 'Deleting existing database'
+rm /vagrant/drtysnow/data/drtysnow.db
+echo '...done'
+
+cd /vagrant/drtysnow/db
+echo 'Generating new Database.'
+python dbsetup.py
+echo '...done'
+
+echo 'Generating fake data'
+python makefakedata.py
+echo '...done'
+
+echo 'installing Database file'
+mv drtysnow.db /vagrant/drtysnow/data
+echo '...done'
+
+cd /vagrant/drtysnow
+echo 'DB has been reset'
